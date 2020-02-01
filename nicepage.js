2b1614cc6d2412fe6bef39093b826f856c02d8fc
wailhlaly <59731361+wailhlaly@users.noreply.github.com>
2020-02-01 18:08:02 +0300

    Update nicepage.js

--- nicepage.js
+++ nicepage.js
@@ -15,109 +15,109 @@
     "userId": 1,
     "id": 3,
     "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    "body": "‏لست شخص سيئ أنا فقط شخص 
+‏جرب جميع الشعور  فـ خُذل !"
   },
   {
     "userId": 1,
     "id": 4,
     "title": "eum et est occaecati",
-    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    "body": "‏طاقة كلام تنتهي بصمت طويل وخيبة أنهُ لن يفهمك أحد."
   },
   {
     "userId": 1,
     "id": 5,
     "title": "nesciunt quas odio",
-    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    "body": "‏كلانا نحب بعضنا البعض بطريقة أنانية."
   },
   {
     "userId": 1,
     "id": 6,
     "title": "dolorem eum magni eos aperiam quia",
-    "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+    "body": "‏"وإني كنت دومًا حذر حتى لا أكسرك، حتى لا تبقى يديك باردة، حتى لا تسهر لوهلة مع حزن قلبك، كنتُ دومًا هنـا لأجلك. ولكن ماذا عنك؟""
   },
   {
     "userId": 1,
     "id": 7,
     "title": "magnam facilis autem",
-    "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+    "body": "‏ما ابي سعة هالأرض احتاج امان ضلوُعك."
   },
   {
     "userId": 1,
     "id": 8,
     "title": "dolorem dolore est ipsam",
-    "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+    "body": "‏ليَت الأيّام مافيها ودَاع."
   },
   {
     "userId": 1,
     "id": 9,
     "title": "nesciunt iure omnis dolorem tempora et accusantium",
-    "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
+    "body": "‏وجهي بشوش وذكرياتي حزينّه ."
   },
   {
     "userId": 1,
     "id": 10,
     "title": "optio molestias id quia eum",
-    "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+    "body": "‏لييَت الحزن مثِل السّهر اخر درُوبه فجر ."
   },
   {
     "userId": 2,
     "id": 11,
     "title": "et ea vero quia laudantium autem",
-    "body": "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
+    "body": "‏الرسَايل الي كانتْ بالثواني صَارت بالتواريّخ."
   },
   {
     "userId": 2,
     "id": 12,
     "title": "in quibusdam tempore odit est dolorem",
-    "body": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+    "body": "‏ينتّهي بعمَري كل شْي إلا حُبك."
   },
   {
     "userId": 2,
     "id": 13,
     "title": "dolorum ut in voluptas mollitia et saepe quo animi",
-    "body": "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
+    "body": "نفسي مره اجوف حب صادق"
   },
   {
     "userId": 2,
     "id": 14,
-    "title": "voluptatem eligendi optio",
-    "body": "fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum"
+    "title": "الشعور يموت يا ناس اذا عطيت الشخص اللي تحبه كل شي مره وحده ، والله ما عاد يحس نفس قبل ، خذوها قاعده مني سواء بنت او شاب ، الحب اللي بينكم والكلام الحب خلوه بين فترات ، وربي راح تحبون بعض للأبد ♥️"
   },
   {
     "userId": 2,
     "id": 15,
     "title": "eveniet quod temporibus",
-    "body": "reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae"
+    "body": "‏الذكْريات اكبَر لعنه بعْد الغيٓاب."
   },
   {
     "userId": 2,
     "id": 16,
     "title": "sint suscipit perspiciatis velit dolorum rerum ipsa laboriosam odio",
-    "body": "suscipit nam nisi quo aperiam aut\nasperiores eos fugit maiores voluptatibus quia\nvoluptatem quis ullam qui in alias quia est\nconsequatur magni mollitia accusamus ea nisi voluptate dicta"
+    "body": "‏تمسكوا بالعلاقة اللي ما فيها كلمة احبك لكن فيها كل شي يثبتها بدون ماتنقال."
   },
   {
     "userId": 2,
     "id": 17,
     "title": "fugit voluptas sed molestias voluptatem provident",
-    "body": "eos voluptas et aut odit natus earum\naspernatur fuga molestiae ullam\ndeserunt ratione qui eos\nqui nihil ratione nemo velit ut aut id quo"
+    "body": "‏يفز قلبُي كل ما اوحيتّ طاريك."
   },
   {
     "userId": 2,
     "id": 18,
     "title": "voluptate et itaque vero tempora molestiae",
-    "body": "eveniet quo quis\nlaborum totam consequatur non dolor\nut et est repudiandae\nest voluptatem vel debitis et magnam"
+    "body": "لقد قيل لي من مصدر موثوق إنني لا أملك قلباً"
   },
   {
     "userId": 2,
     "id": 19,
     "title": "adipisci placeat illum aut reiciendis qui",
-    "body": "illum quis cupiditate provident sit magnam\nea sed aut omnis\nveniam maiores ullam consequatur atque\nadipisci quo iste expedita sit quos voluptas"
+    "body": "لحّظه لقى يا ليتَها كانت سنين."
   },
   {
     "userId": 2,
     "id": 20,
     "title": "doloribus ad provident suscipit at",
-    "body": "qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo"
+    "body": "‏هو بكل الأغانِي أو كل الأغانِي له؟"
   },
   {
     "userId": 3,
