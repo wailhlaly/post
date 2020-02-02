6a0c9901f1b9abdcbf43c219e631b4fd8ba68235
wailhlaly <59731361+wailhlaly@users.noreply.github.com>
2020-02-02 14:15:29 +0300

    Update nicepage.js

--- nicepage.js
+++ nicepage.js
@@ -3,10 +3,7 @@
     "userId": 1,
     "id": 1,
     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-
     "body": "😒😒😒😒😒😒😒😂😂😍"
-
-
   },
   {
     "userId": 1,
