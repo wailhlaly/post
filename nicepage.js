febc7a48732403b7b11b0ab0b2fbff4f5af07efd
wailhlaly <59731361+wailhlaly@users.noreply.github.com>
2020-02-02 14:11:11 +0300

    Update nicepage.js

--- nicepage.js
+++ nicepage.js
@@ -12,7 +12,7 @@
     "userId": 1,
     "id": 2,
     "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    "body": "😁😁😁😁😁😁😁"
   },
   {
     "userId": 1,
